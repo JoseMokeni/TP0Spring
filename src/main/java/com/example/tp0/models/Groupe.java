b7e726ab5fdc172373cfaca2d759789ef316af42
José Mokeni <jmmokeni@gmail.com>
2024-04-02 13:14:27 +0100

    group added and relation implemented

--- src/main/java/com/example/tp0/models/Groupe.java
+++ src/main/java/com/example/tp0/models/Groupe.java
@@ -1,32 +1,28 @@
 package com.example.tp0.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
 @Data
-public class Etudiant {
+public class Groupe {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
+	
 	@Column(length = 40, nullable = false, unique = true)
 	private String nom;
-	private double note;
 	
-	@ManyToOne
-	@JoinColumn(name = "idG")
-	private Groupe gr;
-	
-	
-	
+	@OneToMany(mappedBy = "gr")
+	private List<Etudiant> listeE = new ArrayList<Etudiant>();
 
 }
